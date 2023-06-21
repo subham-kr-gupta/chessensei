2a6a775570aec8351fd54d6d98ad888971c89f4d
Subham Kumar <kumarsubham266@gmail.com>
2023-06-21 17:50:07 +0530

    Update nutrition.py

--- nutrition.py
+++ nutrition.py
@@ -1,14 +1,14 @@
 import openai
 import configparser
 import streamlit as st
-
+import os
 
 @st.cache_data(show_spinner=False)
 def generate_nutrition_plan(name, gender, age, height, food, sports):
     # config = configparser.ConfigParser()
     # config.read("config.ini")
     # openai.api_key = config["OPENAI"]["API_KEY"]
-    openai.api_key = "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"
+    openai.api_key = os.environ["API_KEY"]
     input_text = f"I'm from india, my gender is {gender}, my age is {age}, my height is {height} cm my food preference is {food} can you share me a recommend nutrition plan if am a {sports} player breakfast, lunch, dinner"
 
     try:
